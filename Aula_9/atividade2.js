94be3cedf277ae0ff5ea489c5907ae8a7942db44
Airassilva <airasoares17@gmail.com>
2026-06-18 18:56:02 -0300

    Atividade aula 9

---
+++ Aula_9/atividade2.js
@@ -0,0 +1,29 @@
+// ALGORITMO 1: ESCOVAR OS DENTES
+ 
+// Início
+// Pegar a escova de dentes
+// Colocar pasta de dente na escova
+// Repetir: Escovar uma parte dos dentes; Mover para a próxima parte
+// Enquanto: Ainda houver dentes para escovar
+// Enxaguar a boca
+// Guardar a escova
+// Fim
+
+// O que repete? Escovar uma parte dos dentes
+// Quando continua? Enquanto existir alguma parte dos dentes sem escovar
+// Quando para? Quando todos os dentes estiverem escovados
+// Função principal: EscovarDentes
+ 
+// ALGORITMO 2: ESPERAR O ÔNIBUS
+ 
+// Início
+// Ir até o ponto de ônibus
+// Repetir: Observar os ônibus que chegam; Verificar se é o ônibus desejado
+// Enquanto: O ônibus desejado não chegar
+// Entrar no ônibus
+// Fim
+
+// O que repete? Observar os ônibus e verificar se é o correto
+// Quando continua? Enquanto o ônibus desejado não chegar
+// Quando para? Quando o ônibus desejado chegar ao ponto
+// Função principal: EsperarOnibus
