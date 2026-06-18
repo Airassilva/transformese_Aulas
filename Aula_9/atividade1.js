49a2a0b64be9fc1b7babf217fb84f6f926d3754a
Airassilva <airasoares17@gmail.com>
2026-06-17 21:11:51 -0300

    Aula 9

---
+++ Aula_9/atividade1.js
@@ -0,0 +1,8 @@
+//01 - Repetiu o sorteio de números, verificação se o número já foi sorteado, entrada de dados, armazenamento dos números sorteados, e exibição dos números sorteados.
+//02 - A entrada de dados, condição de repetição, limitação de sorteio dos números, local de armazenamento dos números sorteados.
+//03 - Quando todos os números foram sorteados e teve um ganhador / quando alguém completar o bingo, pq atingiu um o limite de números sorteados.
+//04 - A condição de continuidade: Enquanto houver número para sortear alguém não tiver completado o bingo, o jogo continua. 
+//05 - A condição de parada: Quando todos os números forem sorteados ou quando alguém completar o bingo, o jogo para.
+//06 - Uma função para cada ação que se repete: o sorteio de números, a verificação de repetição, a verificação do ganhador.
+//07 - O codigo sorteia o número 75 vezes
+//08 - repete até que alguém complete o bingo ou até que todos os números sejam sorteados, o que acontecer primeiro.
